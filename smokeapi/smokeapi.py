116660f28b30e1088916e5cc55496ceb9309a569
AWegnerGitHub <andy@andrewwegner.com>
2016-10-09 23:05:20 -0500

    Adds send_data method for interacting with write routes

--- smokeapi/smokeapi.py
+++ smokeapi/smokeapi.py
@@ -26,6 +26,9 @@ class SmokeAPI(object):
 
             :param key: (string) **(Required)** A valid API key. An API key can be received by following the
                 current instructions in the `API Documentation <https://github.com/Charcoal-SE/metasmoke/wiki/API-Documentation>`__.
+            :param token: (string) **(Required for write access/Optional is no write routes are called)**
+                This is a valid write token retreived by following instructions in the `API Documentation <https://github.com/Charcoal-SE/metasmoke/wiki/API-Documentation>`__.
+                If this is not set, calls to `send_data` will fail.
             :param proxy: (dict) (optional) A dictionary of http and https proxy locations
                 Example:
 
@@ -49,6 +52,7 @@ class SmokeAPI(object):
         self.max_pages = kwargs.get('max_pages', 5)
         self.per_page = kwargs.get('per_page', 100)
         self._api_key = key
+        self.token = kwargs.get('token', None)
         self._endpoint = None
         self._previous_call = None
         self._base_url = 'https://metasmoke.erwaysoftware.com/api/'
@@ -56,7 +60,7 @@ class SmokeAPI(object):
     def __repr__(self):
         return "<SmokeAPI> endpoint: {}  Last URL: {}".format(self._endpoint, self._previous_call)
 
-    def fetch(self, ids, endpoint=None, page=1, **kwargs):
+    def fetch(self, endpoint=None, page=1, **kwargs):
         """Returns the results of an API call.
 
             This is the main work horse of the class. It builds the API query
@@ -103,10 +107,6 @@ class SmokeAPI(object):
                   "page": page,
                   "key": self._api_key
                   }
-
-        # TODO: Test with an access token
-        # if self.access_token:
-        #     params['access_token'] = self.access_token
 
         # This block will replace {ids} placeholds in end points
         # converting .fetch('posts/{ids}', ids=[222, 1306, 99999]) to
@@ -165,6 +165,77 @@ class SmokeAPI(object):
             else:
                 break
 
+        r = []
+        for d in data:
+            r.extend(d['items'])
+        items = list(chain(r))
+        result = {'has_more': data[-1]['has_more'],
+                  'page': params['page'],
+                  'total': len(items),
+                  'items': items}
+
+        return result
+
+    def send_data(self, endpoint=None,  **kwargs):
+        """Sends data to the API.
+
+        This call is similar to ``fetch``, but **sends** data to the API instead
+        of retrieving it.
+
+        Returned data will appear in the ``items`` key of the resulting
+        dictionary.
+
+        Sending data **requires** that the ``token`` is set.
+
+        :param endpoint: (string) **(Required)** The API end point being called. Available endpoints are listed on
+            the official `API Documentation <https://github.com/Charcoal-SE/metasmoke/wiki/API-Documentation>`__.
+
+            If no end point is passed, a ``ValueError`` will be raised
+        :param kwargs: Parameters accepted by individual endpoints. These parameters
+            **must** be named the same as described in the endpoint documentation
+        :rtype: (dictionary) A dictionary containing wrapper data regarding the API call
+            and the results of the call in the `items` key. If multiple
+            pages were received, all of the results will appear in the
+            ``items`` tag.
+        """
+        if not endpoint:
+            raise ValueError('No end point provided.')
+
+        if not self.token:
+            raise ValueError('A write token has not been set. This is required for all MetaSmoke API routes. This can\n'
+                             'be set by setting the "token" parameter of your SmokeAPI object.')
+
+        self._endpoint = endpoint
+
+        params = {
+            "key": self._api_key,
+            "token": self.token
+        }
+
+        if 'ids' in kwargs:
+            ids = ';'.join(str(x) for x in kwargs['ids'])
+            kwargs.pop('ids', None)
+        else:
+            ids = None
+
+        params.update(kwargs)
+
+        data = []
+
+        base_url = "{}{}/".format(self._base_url, endpoint)
+        response = requests.post(base_url, data=params, proxies=self.proxy)
+        self._previous_call = response.url
+        response = response.json()
+
+        try:
+            code = response["error_code"]
+            name = response["error_name"]
+            message = response["error_message"]
+            raise SmokeAPIError(self._previous_call, code, name, message)
+        except KeyError:
+            pass  # This means there is no error
+
+        data.append(response)
         r = []
         for d in data:
             r.extend(d['items'])
